6cd1ef5711229aad9b412809492a26ca0d4ec954
Tasnimul H. Tauhid <thtauhid.71@gmail.com>
2024-02-04 16:32:31 +0530

    Remove console.log statement

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -17,8 +17,6 @@ export default async function DashboardPage() {
     },
   });
 
-  console.log(user);
-
   return (
     <div className="">
       <h1 className="font-bold text-2xl border-b border-b-stone-400 p-4">
