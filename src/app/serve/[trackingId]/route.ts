23521d3265c0af3701e9e71277456ad627c3004f
Tasnimul H. Tauhid <thtauhid.71@gmail.com>
2024-02-18 01:11:04 +0530

    Refactor image fetching and response headers

--- src/app/serve/[trackingId]/route.ts
+++ src/app/serve/[trackingId]/route.ts
@@ -14,37 +14,56 @@ export async function GET(
       data: { opens: { increment: 1 } },
     });
 
-    let url = "https://picsum.photos/200";
+    // let url = "https://picsum.photos/200";
 
-    let contentType, contentLength, h;
+    // let contentType, contentLength, h;
 
-    const stream = await fetch(url).then((r) => {
-      h = r.headers;
-      contentType = r.headers.get("content-type");
-      contentLength = r.headers.get("content-length");
+    // const stream = await fetch(url).then((r) => {
+    //   h = r.headers;
+    //   contentType = r.headers.get("content-type");
+    //   contentLength = r.headers.get("content-length");
 
-      const reader = r.body?.getReader();
-      return new ReadableStream({
-        start(controller) {
-          return pump();
-          async function pump() {
-            const { done, value } = await reader!.read();
-            // When no more data needs to be consumed, close the stream
-            if (done) {
-              controller.close();
-              return;
-            }
-            // Enqueue the next data chunk into our target stream
-            controller.enqueue(value);
-            return pump();
-          }
-        },
-      });
+    //   const reader = r.body?.getReader();
+    //   return new ReadableStream({
+    //     start(controller) {
+    //       return pump();
+    //       async function pump() {
+    //         const { done, value } = await reader!.read();
+    //         // When no more data needs to be consumed, close the stream
+    //         if (done) {
+    //           controller.close();
+    //           return;
+    //         }
+    //         // Enqueue the next data chunk into our target stream
+    //         controller.enqueue(value);
+    //         return pump();
+    //       }
+    //     },
+    //   });
+    // });
+
+    // a 5x5 pixel pink gif
+    const gif = Buffer.from(
+      "R0lGODlhBwAHAIAAAP///wAAACH5BAEAAAAALAAAAAAHAAcAAAIPlI+py+0Po5yUFQA7",
+      "base64"
+    );
+
+    const stream = new ReadableStream({
+      start(controller) {
+        controller.enqueue(gif);
+        controller.close();
+      },
     });
 
-    const headers = new Headers(h);
+    // const headers = new Headers(h);
+    // headers.set("Cache-Control", REVALIDATE);
+    // headers.set("Content-Type", "image/jpeg");
+    // headers.set("Content-Length", String(gif.length));
+
+    const headers = new Headers();
     headers.set("Cache-Control", REVALIDATE);
-    headers.set("Content-Type", "image/jpeg");
+    headers.set("Content-Type", "image/gif");
+    headers.set("Content-Length", String(gif.length));
 
     return new NextResponse(stream, {
       status: 200,
